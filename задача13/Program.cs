eff13654bf52762e80bbccd51207277795de7382
Lena <imagine.youeself19@gmail.com>
2022-06-28 12:20:05 +0300

    добавлено одно из решений задачи 13

---
+++ задача13/Program.cs
@@ -0,0 +1,9 @@
+﻿int n = new Random().Next(0, 10000);
+Console.WriteLine($"Выбрано число {n}");
+if (n < 100){
+    Console.WriteLine("Третьей цифры нет");
+}
+else{
+    int thirdDigit = n % 100 / 10;
+    Console.WriteLine($"Третья цифра заданного числа {n} равна {thirdDigit}");
+}
