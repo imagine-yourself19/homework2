fa249a367ae97c7c0e89b7e235778d59b56bf2ef
Lena <imagine.youeself19@gmail.com>
2022-06-28 12:40:05 +0300

    добавлена задача15

---
+++ задача15/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Напишите число от 1 до 7 для определения дня недели");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 6 || n== 7){
+    Console.WriteLine("Ура, выходной!");
+}
+else if (n < 1 || n > 7) {
+    Console.WriteLine("Ошибка ввода, введите число от 1 до 7");
+}
+else {
+    Console.WriteLine("Будний день :(");
+}
